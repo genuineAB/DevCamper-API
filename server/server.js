f2cd494a9ea47b3c9c2892a59138c025df976a23
genuineAB <bamigboyeabiola@gmail.com>
2022-11-10 17:14:40 +0100

    Setting up express router

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 
+//Route Files
+const bootcamps = require('./routes/bootcamps')
+
 // Load env vars
 // dotenv.config({path: '../config/.env'});
 
 const app = express();
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT;
 
