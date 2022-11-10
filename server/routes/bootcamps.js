da936bb095c12483ca8a0021bfae5c6078cd2b0f
genuineAB <bamigboyeabiola@gmail.com>
2022-11-10 18:16:59 +0100

    Creating Middleware

--- server/routes/bootcamps.js
+++ server/routes/bootcamps.js
@@ -14,29 +14,6 @@ router
     .patch(updateBootCamp)
     .delete(deleteBootCamp);
 
-// //GET ALL BOOTCAMPS
-// router.get('/', (req, res) => {
-//     res.status(200).json({success: true, msg: 'Show all bootcamps'});
-// });
 
-// //GET A BOOTCAMP
-// router.get('/:id', (req, res) => {
-//     res.status(200).json({success: true, msg: 'Show Single Bootcamp'});
-// });
-
-// //CREATE BOOTCAMP
-// router.post('/', (req, res) => {
-//     res.status(200).json({success: true, msg: 'Create a New Bootcamp'});
-// });
-
-// //UPDATE A BOOTCAMP
-// router.patch('/:id', (req, res) => {
-//     res.status(200).json({success: true, msg: 'Update Bootcamp'});
-// });
-
-// //DELETE A BOOTCAMP
-// router.delete('/:id', (req, res) => {
-//     res.status(200).json({success: true, msg: 'Delete Bootcamp'});
-// });
 
 module.exports = router; 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const morgan = require('morgan');
 
 //Route Files
 const bootcamps = require('./routes/bootcamps')
@@ -8,6 +9,11 @@ const bootcamps = require('./routes/bootcamps')
 // dotenv.config({path: '../config/.env'});
 
 const app = express();
+
+//Dev Logging Middleware
+if(process.env.NODE_ENV ==='development'){
+    app.use(morgan('dev'));
+}
 
 // Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
