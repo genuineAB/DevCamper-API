dbbf71ac30c20174a9e10bd79b5184409ddff9a0
genuineAB <bamigboyeabiola@gmail.com>
2022-11-12 12:59:05 +0100

    Connecting App to Database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 require('dotenv').config();
 const morgan = require('morgan');
+const connectDB = require('../config/db');
+const colors = require('colors');
 
 //Route Files
 const bootcamps = require('./routes/bootcamps')
 
 // Load env vars
 // dotenv.config({path: '../config/.env'});
+
+// Connect to DataBase
+connectDB();
 
 const app = express();
 
@@ -21,6 +26,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT;
 
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+const server = app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
+});
+
+//Handle Unhandled Promise Rejection
+process.on('unhandledRejection', (error, promise) => {
+    console.log(`Error: ${error.message}`.red);
+
+    //Close Server and Exit Process
+    server.close(() => process.exit(1))
 })
